8f4151f4a581a9c7f63a2aa7b60ab4a72dcfe26a
Jeeya Patil <jeeyapatil2005@gmail.com>
2025-07-05 15:56:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import './App.css';
+import './styles/pages.css';
+import { useState } from "react";
+import DeveloperInfoPopup from './components/DeveloperInfo/DeveloperInfoPopup';
+import ChatbotComponent from './components/Chatbot/ChatbotComponent';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import CoursesPage from './pages/CoursesPage';
+import ContactPage from './pages/ContactPage';
+import AdmissionPage from './pages/AdmissionPage';
+const App = () => {
+  const [showPopup, setShowPopup] = useState(true);
+
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
+  return(
+    
+    <div>
+      <div>
+        {/* Your main application content */}
+        <DeveloperInfoPopup
+          show={showPopup}
+          onClose={handleClosePopup}
+          studentName="Nikita Prakash Mane"
+          studentPhotoUrl="/images/nikita-image.png" // Path to their photo
+          uniqueMessage="Learned so much during this OJT! This app showcases my independent coding and deployment skills"
+        />
+      </div>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage/>}></Route>
+          <Route path="/home" element={<HomePage/>}></Route>
+          <Route path="/about" element={<AboutPage/>}></Route>
+          <Route path="/courses" element={<CoursesPage/>}></Route>
+          <Route path="/contact" element={<ContactPage/>}></Route>
+          <Route path="/admission" element={<AdmissionPage/>}></Route>
+        </Routes>
+        <ChatbotComponent/>
+      </Router>
+    </div>
+  )
+}
+export default App;
